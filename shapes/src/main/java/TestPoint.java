830071cd81f8608f7a9b52476d57b6ab847e2b1e
HengyiGuo <56237115+HengyiGuo@users.noreply.github.com>
2020-01-15 02:58:52 +0100

    update

---
+++ shapes/src/main/java/TestPoint.java
@@ -0,0 +1,14 @@
+package fr.dauphine.ja.GUOHengyi.shapes;
+
+import Model.Point;
+import Model.Ring;
+
+public class TestPoint
+{
+	public static void main(String[] args)
+	{
+		
+		Ring r=new Ring(new Point(2,3),5,3);
+		System.out.println(r);
+	}
+}
