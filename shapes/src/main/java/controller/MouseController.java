3a6416eb3383142fa64205993640156a48e13b40
HengyiGuo <56237115+HengyiGuo@users.noreply.github.com>
2020-01-15 03:14:13 +0100

    update controller

--- shapes/src/main/java/controller/MouseController.java
+++ shapes/src/main/java/controller/MouseController.java
@@ -1,1 +1,40 @@
+package controller;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.JFrame;
+
+import Model.Shape;
+import Model.World;
+import Model.Point;
+
+public class MouseController implements MouseMotionListener 
+{
+	World w;
+	JFrame frame;
+	public MouseController(World _w, JFrame _frame)
+	{
+		this.w=_w;
+		this.frame=_frame;
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) 
+	{
+		/*Point p=new Point(e.getX(),e.getY());
+		for(Shape s:w.getShapes())
+		if(s.contains(p))
+		{
+			s.translate(e.getX(),e.getY());
+			frame.repaint();
+		}*/
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) 
+	{
+		w.getShapes().get(w.getShapes().size()-2).translate(e.getX(), e.getY());
+		frame.repaint();
+	}
+
+}
